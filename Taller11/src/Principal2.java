4fd36dfe41783d8c6bf8d4e7bb726b1003059255
JSebastian2002 <jspenarreta@utpl.edu.ec>
2022-07-02 11:14:19 -0500

    Taller 11

--- Taller11/src/Principal2.java
+++ Taller11/src/Principal2.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package taller11;
 
 import java.util.ArrayList;
 
